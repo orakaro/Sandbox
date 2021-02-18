8a15a42aadddcc397af86b3313b330a37917789c
orakaro <nhatminh_179@hotmail.com>
2021-02-18 11:15:01 +0900

    SwiftUI Sandbox

---
+++ SwiftUISandbox/SimpleView.swift
@@ -0,0 +1,20 @@
+//
+//  SimpleView.swift
+//  SwiftUISandbox
+//
+//  Created by DTVD on 2021/02/18.
+//
+
+import SwiftUI
+
+struct SimpleView: View {
+    var body: some View {
+        Text("hello")
+    }
+}
+
+struct SimpleView_Previews: PreviewProvider {
+    static var previews: some View {
+        SimpleView()
+    }
+}
---
+++ SwiftUISandbox/SwiftUISandbox/SwiftUISandbox.h
@@ -0,0 +1,18 @@
+//
+//  SwiftUISandbox.h
+//  SwiftUISandbox
+//
+//  Created by DTVD on 2021/02/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUISandbox.
+FOUNDATION_EXPORT double SwiftUISandboxVersionNumber;
+
+//! Project version string for SwiftUISandbox.
+FOUNDATION_EXPORT const unsigned char SwiftUISandboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUISandbox/PublicHeader.h>
+
+
